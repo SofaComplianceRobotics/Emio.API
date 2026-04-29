ae7f5b5ca6dd31fc398e0dfa2224dfcd1683d2d1
HanaeRateau <hanaerateau@yahoo.fr>
2026-04-29 18:55:29 +0200

    Integrates a UDP bridge and changes to be compatible with it

---
+++ emioapi/udp_bridge/udp_bridge_params.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# --- Configurable settings ---
+fps = 30
+nb_markers = 1
+side = "plan"  # "top", "front"
+sort = "y"  # "y" or "z", only for front camera
+
+# UDP settings
+remote_ip    = "127.0.0.1"
+remote_port  = 25000
+local_port   = 25001
+bind_port    = 9090
+recv_timeout = 0.1
+
+# --- Fixed settings (DO NOT CHANGE) ---
+ny = 3 * nb_markers # number of measurements
+nu = 4 # number of actuators
+
+# camera settings
+depth = 249 # for front camera, 
+plane_d = 5
+plane_n = np.array([1,0,0])
+front2top_offset = np.array([-2.2, 195.8, -254])
